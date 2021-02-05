6296b46bfd15a4caf76cb0d24759f5e18352bfdf
broveloper <broveloper@gmail.com>
2021-02-05 17:11:06 -0500

    device detect

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,17 +5,25 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import HelpOutline from '@material-ui/icons/HelpOutline';
 import Tooltip from '@material-ui/core/Tooltip';
+import { isMacOs, isMobile, isWindows } from 'react-device-detect';
 import { Page } from 'components/Page';
 import theme from './theme';
 
 export const App = () => {
+
+	const tip =
+		isMobile ? 'Longpress to see scripture.' :
+		isWindows ? 'Hold down ⊞ to see scripture.' :
+		isMacOs ? 'Hold down ⌘ to see scripture.' :
+		'Click and hold to see scripture.';
+
   return <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <Container maxWidth="sm">
 			<Box my={4}>
 				<Typography variant="h5" component="h1" gutterBottom>
-					Membroize <Tooltip title="Hold down ⌘ for help."><HelpOutline color="secondary" fontSize="small" /></Tooltip>
+					Membroize <Tooltip title={tip}><HelpOutline color="secondary" fontSize="small" /></Tooltip>
 				</Typography>
 				<Page />
 			</Box>
