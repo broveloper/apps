cff4f97a2797ecf4b32b44191c8decd2c4cc5b72
broveloper <broveloper@gmail.com>
2021-02-19 15:37:37 -0500

    cleaning

--- src/components/App/AppBarTop.js
+++ src/components/App/AppBarTop.js
@@ -1,10 +1,9 @@
 import Box from '@material-ui/core/Box';
 import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Brightness4Icon from '@material-ui/icons/Brightness4';
-import Brightness7Icon from '@material-ui/icons/Brightness7';
-import { useApp } from '@utils/useApp';
-import { AppBarTheme, AppContainer } from 'components/App';
+// import Brightness4Icon from '@material-ui/icons/Brightness4';
+// import Brightness7Icon from '@material-ui/icons/Brightness7';
+// import { useApp } from '@utils/useApp';
+import { AppContainer } from 'components/App';
 
 
 export const AppBarTop = props => {
@@ -12,15 +11,15 @@ export const AppBarTop = props => {
 		children,
 		...rest
 	} = props;
-	const {
-		setTheme,
-		theme,
-	} = useApp();
+	// const {
+	// 	setTheme,
+	// 	theme,
+	// } = useApp();
 
-	const handleBrightness = () => setTheme(theme === 'light' ? 'dark' : 'light');
-	const BrightnessIcon = theme === 'light'
-		? Brightness4Icon
-		: Brightness7Icon;
+	// const handleBrightness = () => setTheme(theme === 'light' ? 'dark' : 'light');
+	// const BrightnessIcon = theme === 'light'
+	// 	? Brightness4Icon
+	// 	: Brightness7Icon;
 
 	return <Box {...rest}>
 		<AppContainer>
