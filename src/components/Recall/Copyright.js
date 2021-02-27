ddac8d2f8ee125266bf10f219a1a0ed648ddcb68
broveloper <broveloper@gmail.com>
2021-02-27 08:33:32 -0500

    add nkjv

--- src/components/Recall/Copyright.js
+++ src/components/Recall/Copyright.js
@@ -1,11 +1,18 @@
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import { usePassage } from '@utils/useApp';
+const useStyles = makeStyles({
+	root: {
+		whiteSpace: 'pre-wrap',
+	}
+});
 
 export const Copyright = () => {
 	const { bible } = usePassage();
+	const classnames = useStyles();
 	return <>
-		<Box px={3} py={6}>
+		<Box className={classnames.root} px={3} py={6}>
 			<Typography
 				align="center"
 				component="div"
